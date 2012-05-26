ec039b43f9ac0bed5a3d7fb33ebcc77e5395a210
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2012-05-26 18:04:02 +0200

    Readme angepasst

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 Software zur Verwaltung des Aktionsplans Flora der Fachstelle Naturschutz des Kantons Zürich.
-Ist bloss ein Versuch, ob die Idee funktioniert.
+Bisher auf Microsoft Access, das über ODBC auf eine MySql-Datenbank im Netz greift.
+
+#Ziel
+- Die Daten können von ausserhalb und endlich auch innerhalb der Fachstelle Naturschutz bearbeitet werden
+- Einfachere und übesichtlichere Benutzerführung
+- Keine Installation, keine Installationskosten, automatische Updates
+- Schlankes, weniger komplexes Gesamtsystem mit weniger Abhängigkeiten (läuft auch auf Mac und Linux)
+- Darstellung und Lokalisierung auf Luftbildern direkt in der Anwendung
+
+#Idee
+- Steuerung zu 100% über einen dynamisch aufgebauten Strukturbaum. Er ersetzt diese bisherigen Objekte: Baum, Register, Suchfelder und Verschiebe-/Kopierbefehle
+- Rechts neben dem Baum werden die Daten der gewählten Struktur (= "node") angezeigt, z.B. Teilpopulation oder Feldkontrolle.
+- Jeder node enthält die Informationen, mit denen er sich bei Aktivierung seine Daten aus der DB holt > einfachste Abfragen, hoffentlich kurze Ladezeiten
+- Nodes wie Teilpopulationen und Feldkontrollen können im Baum verschoben oder kopiert werden
+- Steuerung mit Ajax für schnelle Ladezeiten
+- Suchfunktion im Baum?
+- eigenes Symbol für AP-Arten, farblich nach Status abgestuft?
+
+#Einschränkungen
+- Abfragen und Exporte vorläufig noch in Access ausführen (muss wohl sowieso flexibilisiert werden)
+- Artbeobachtungen vorläufig noch in Access zuweisen
+- Artbeobachtungen aus EvAB ev. länger weiterhin in Access zuweisen (oder Uploadtool und Zuweisung im Web?)
+- Berichte vermutlich langfristig weiterhin in Access erstellen (aufwändig und schwierig umzusetzen)
+
+#Roadmap
+Ist momentan bloss ein Versuch, ob die Idee funktioniert.
--- ap.php
+++ ap.php
@@ -9,36 +9,10 @@ mysql_select_db("alexande_apflora") or die("Auswahl der Datenbank fehlgeschlagen
 $query = "SELECT Name, ApArtId FROM qryAp1";
 $result = mysql_query($query) or die("Anfrage fehlgeschlagen: " . mysql_error());
 
-// in json ausgeben
-/*$rows = array();
-while($r = mysql_fetch_assoc($result)) {
-	//$a = '{ "data":"'.$r['Name'].'", "attr": {"ApArtId":'.json_encode($r['ApArtId']).'}}';
-	$a = '{ "data":"';
-	$b = $r['Name'];
-	$c = '", "attr": {"id":"';
-	$d = $r['ApArtId'];
-	$e = '"}, "state":"closed", "children":["Populationen", "AP-Ziele"]},';
-    $rows = $a.$b.$c.$d.$e;
-}*/
-
-/*$rows = "";
-while($r = mysql_fetch_assoc($result)) {
-	//$a = '{ "data":"'.$r['Name'].'", "attr": {"ApArtId":'.json_encode($r['ApArtId']).'}}';
-	$a = '{"data":"';
-	$b = $r['Name'];
-	$c = '", "attr": {"id":"';
-	$d = $r['ApArtId'];
-	$e = '"}, "state":"closed", "children":["Populationen", "AP-Ziele"]}';
-	if ($rows) {
-    	$rows .= ",";
-    }
-    $rows .= $a.$b.$c.$d.$e;
-}*/
-
 $rows = "[";
 while($r = mysql_fetch_assoc($result)) {
 	$attr = array("id" => $r['ApArtId']);
-	$children = array(0 => "Populationen", 1 => "AP-Ziele");
+	$children = array(0 => "Populationen", 1 => "AP-Ziele", 2 => "Erfolgskriterien", 3 => "AP-Berichte", 4 => "Berichte");
 	$row = array("data" => $r['Name'], "attr" => $attr, "state" => "closed", "children" => $children);
 	if ($rows != "[") {
     	$rows .= ",";
@@ -47,11 +21,20 @@ while($r = mysql_fetch_assoc($result)) {
 }
 $rows .= "]";
 
-//echo $rows;
+/*$rows = "[";
+while($r = mysql_fetch_assoc($result)) {
+	$attr = array("id" => $r['ApArtId']);
+	$data = array("title" => $r['Name'], "attr" => $attr);
+	$children = array(0 => "Populationen", 1 => "AP-Ziele", 2 => "Erfolgskriterien", 3 => "AP-Berichte", 4 => "Berichte");
+	$row = array($data, "state" => "closed", "children" => $children);
+	if ($rows != "[") {
+    	$rows .= ",";
+    }
+    $rows .= json_encode($row);
+}
+$rows .= "]";*/
+
 print($rows);
-//print($result);
-//print json_encode($rows);
-//print json_encode($result);
 
 // Resultset freigeben
 mysql_free_result($result);
