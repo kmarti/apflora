502065faf6f5614e535a994b27fcdd93270aa34e
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2012-05-26 13:47:39 +0200

    Baum wird jetzt angezeigt

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-apflora
-=======
+Software zur Verwaltung des Aktionsplans Flora der Fachstelle Naturschutz des Kantons Zürich.
+Ist bloss ein Versuch, ob die Idee funktioniert.
--- ap.php
+++ ap.php
@@ -10,25 +10,47 @@ $query = "SELECT Name, ApArtId FROM qryAp1";
 $result = mysql_query($query) or die("Anfrage fehlgeschlagen: " . mysql_error());
 
 // in json ausgeben
-$rows = array();
-/*while($r = mysql_fetch_assoc($result)) {
+/*$rows = array();
+while($r = mysql_fetch_assoc($result)) {
 	//$a = '{ "data":"'.$r['Name'].'", "attr": {"ApArtId":'.json_encode($r['ApArtId']).'}}';
 	$a = '{ "data":"';
 	$b = $r['Name'];
-	$c = '", "attr": {"ApArtId":"';
+	$c = '", "attr": {"id":"';
 	$d = $r['ApArtId'];
-	$e = '"}}';
-    $rows[] = $a.$b.$c.$d.$e;
+	$e = '"}, "state":"closed", "children":["Populationen", "AP-Ziele"]},';
+    $rows = $a.$b.$c.$d.$e;
 }*/
+
+/*$rows = "";
 while($r = mysql_fetch_assoc($result)) {
-    $rows[] = $r['Name'];
-}
-
-/*while($r = mysql_fetch_assoc($result)) {
-    $rows[] = $r;
+	//$a = '{ "data":"'.$r['Name'].'", "attr": {"ApArtId":'.json_encode($r['ApArtId']).'}}';
+	$a = '{"data":"';
+	$b = $r['Name'];
+	$c = '", "attr": {"id":"';
+	$d = $r['ApArtId'];
+	$e = '"}, "state":"closed", "children":["Populationen", "AP-Ziele"]}';
+	if ($rows) {
+    	$rows .= ",";
+    }
+    $rows .= $a.$b.$c.$d.$e;
 }*/
 
-print json_encode($rows);
+$rows = "[";
+while($r = mysql_fetch_assoc($result)) {
+	$attr = array("id" => $r['ApArtId']);
+	$children = array(0 => "Populationen", 1 => "AP-Ziele");
+	$row = array("data" => $r['Name'], "attr" => $attr, "state" => "closed", "children" => $children);
+	if ($rows != "[") {
+    	$rows .= ",";
+    }
+    $rows .= json_encode($row);
+}
+$rows .= "]";
+
+//echo $rows;
+print($rows);
+//print($result);
+//print json_encode($rows);
 //print json_encode($result);
 
 // Resultset freigeben
