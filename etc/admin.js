9fa31c32e3a096f7340f0a53ed716e80ee2ad6c8
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2013-01-11 11:19:54 +0100

    Dateistruktur verbessert

---
+++ etc/admin.js
@@ -0,0 +1,23 @@
+"barbalex.ch", "alexande", "excalibu"
+ersetzen mit:
+"127.0.0.1", "root", "admin"
+
+
+"barbalex.ch", "alexande", "excalibu", "alexande_apflora"
+//ersetzen mit:
+"127.0.0.1", "root", "admin", "alexande_apflora"
+
+
+
+
+"barbalex.ch", "alexande", "excalibu", "alexande_beob"
+//ersetzen mit:
+"127.0.0.1", "root", "admin", "alexande_beob"
+
+
+
+
+http://www.apflora.barbalex.ch
+http://192.168.0.192:80/apflora
+
+
