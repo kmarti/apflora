091f7020cd85b40ecfaaf1a3e85d22d083feddae
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2013-09-13 14:50:34 +0200

    tblBeobBereitgestellt und die benötigten Abfragen erstellt

--- sql/ViewsGenerieren.sql
+++ sql/ViewsGenerieren.sql
@@ -1121,6 +1121,26 @@ WHERE vPopBer.`Pop Guid`=tblPopulation.PopGuid AND vPopMassnBer.`Pop Guid`=tblPo
 ORDER BY ArtenDb_tblFloraSisf.Name, tblPopulation.PopNr, tblTeilpopulation.TPopNr;
 
 
+*****************************************************
+
+Bereitgestellte Beobachtungen löschen:
+DELETE FROM tblBeobBereitgestellt
+
+Beobachtungen von Info Spezies bereitstellen:
+INSERT INTO tblBeobBereitgestellt (NO_NOTE, NO_ISFS, Datum, Autor) SELECT NO_NOTE, NO_ISFS, IF(M_NOTE IS NULL, A_NOTE, IF(J_NOTE IS NULL, CONCAT(A_NOTE, ".", IF(LENGTH(M_NOTE) = 2, M_NOTE, CONCAT("0", M_NOTE))), CONCAT(A_NOTE, ".", IF(LENGTH(M_NOTE) = 2, M_NOTE, CONCAT("0", M_NOTE)), ".", IF(LENGTH(J_NOTE) = 2, J_NOTE, CONCAT("0", J_NOTE))))), IF(PRENOM_PERSONNE_OBS IS NULL, NOM_PERSONNE_OBS, CONCAT(PRENOM_PERSONNE_OBS, " ", NOM_PERSONNE_OBS)) FROM tblBeobInfoSpezies WHERE ZH_GUID IS NULL;
+
+Dabei wurden ersetzt:
+M_NOTE mit: IF(LENGTH(M_NOTE) = 2, M_NOTE, CONCAT("0", M_NOTE))
+J_NOTE mit: IF(LENGTH(J_NOTE) = 2, J_NOTE, CONCAT("0", J_NOTE))
+
+Beobachtungen von EvAB bereitstellen:
+INSERT INTO tblBeobBereitgestellt (NO_NOTE_PROJET, NO_ISFS, Datum, Autor) SELECT NO_NOTE_PROJET, NO_ISFS, IF(M_NOTE IS NULL, A_NOTE, IF(J_NOTE IS NULL, CONCAT(A_NOTE, ".", IF(M_NOTE>0, IF(LENGTH(M_NOTE) = 2, M_NOTE, CONCAT("0", M_NOTE)), NULL)), CONCAT(A_NOTE, ".", IF(M_NOTE>0, IF(LENGTH(M_NOTE) = 2, M_NOTE, CONCAT("0", M_NOTE)), NULL), ".", IF(J_NOTE>0, IF(LENGTH(J_NOTE) = 2, J_NOTE, CONCAT("0", J_NOTE)), NULL)))), IF(PRENOM_PERSONNE_OBS IS NULL, NOM_PERSONNE_OBS, CONCAT(PRENOM_PERSONNE_OBS, " ", NOM_PERSONNE_OBS)) FROM tblBeobEvab;
+
+Dabei wurden ersetzt:
+M_NOTE mit: IF(M_NOTE>0, IF(LENGTH(M_NOTE) = 2, M_NOTE, CONCAT("0", M_NOTE)), NULL)
+J_NOTE mit: IF(J_NOTE>0, IF(LENGTH(J_NOTE) = 2, J_NOTE, CONCAT("0", J_NOTE)), NULL)
+weil die Felder statt Nullwerten eine 0 enthalten!
+
 
 
 
