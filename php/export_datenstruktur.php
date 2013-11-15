0b3631e58c5768c46cf935274a1f7996ef8fe5e6
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2013-11-15 14:04:28 +0100

    uups, einen hab ich noch...

--- php/export_datenstruktur.php
+++ php/export_datenstruktur.php
@@ -1,7 +1,7 @@
 <?php
 // Verbindung aufbauen, Datenbank auswählen
 
-$link = new mysqli("apflora.ch", "alexande", "y3oYksFsQL49es9x", "information_schema");
+$link = new mysqli("localhost", "alexande", "y3oYksFsQL49es9x", "information_schema");
 
 /* check connection */
 if ($link->connect_errno) {
