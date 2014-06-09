78346379708304e71147464927535e8bb26dc288
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-06-09 21:02:48 +0200

    gulp: css und js optimiert

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,19 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
     notify = require('gulp-notify');
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('style', 'scripts');
+gulp.task('default', function() {
+    gulp.start('styles', 'scripts');
 });
 
-gulp.task('style', function() {
+gulp.task('styles', function() {
     return gulp.src('style/apflora.css')
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(minifycss())
-        .pipe(gulp.dest('dist/style'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('style'))
         .pipe(notify({message: 'css task beendet'}));
 });
 
@@ -25,11 +27,6 @@ gulp.task('scripts', function() {
     return gulp.src(['src/jquery-migrate.js', 'src/jquery-ui.js', 'src/jquery.ui.touch-punch.js', 'src/jquery.cookie.js', 'src/jquery.hotkeys.js', 'src/hammer.js', 'src/jquery.hammer.js', 'src/markerclusterer.js', 'src/markerwithlabel.js', 'src/ruler.js', 'src/jsuri.js', 'src/shapefile/shapefile.js', 'src/shapefile/stream.js', 'src/shapefile/dbf.js', 'src/apflora.js'])
         .pipe(concat('main.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/src'))
+        .pipe(gulp.dest('src'))
         .pipe(notify({ message: 'Scripts task beendet' }));
-});
-
-gulp.task('clean', function() {
-    return gulp.src(['dist/img', 'dist/kml', 'dist/php', 'dist/src', 'dist/style'], {read: false})
-        .pipe(clean());
 });
