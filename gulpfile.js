b8dd142f7beb9272e48d348a8a844d43734d9ea9
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-06-09 21:58:18 +0200

    gulp: sftp upload eingerichtet

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    notify = require('gulp-notify');
+    notify = require('gulp-notify'),
+    sftp = require('gulp-sftp');
 
 gulp.task('default', function() {
     gulp.start('styles', 'scripts');
@@ -29,4 +30,52 @@ gulp.task('scripts', function() {
         .pipe(uglify())
         .pipe(gulp.dest('src'))
         .pipe(notify({ message: 'Scripts task beendet' }));
+});
+
+gulp.task('sftp', function() {
+    gulp.start('sftp_', 'sftp_src', 'sftp_style', 'sftp_php');
+});
+
+gulp.task('sftp_src', function () {
+    return gulp.src('src/*')
+        .pipe(sftp({
+            host: '146.185.161.6',
+            auth: 'keyMain',
+            authFile: '.ftppass',
+            port: 30000,
+            remotePath: '/var/zpanel/hostdata/zadmin/public_html/apflora_ch/src'
+        }));
+});
+
+gulp.task('sftp_style', function () {
+    return gulp.src('style/*')
+        .pipe(sftp({
+            host: '146.185.161.6',
+            auth: 'keyMain',
+            authFile: '.ftppass',
+            port: 30000,
+            remotePath: '/var/zpanel/hostdata/zadmin/public_html/apflora_ch/style'
+        }));
+});
+
+gulp.task('sftp_php', function () {
+    return gulp.src('php/*')
+        .pipe(sftp({
+            host: '146.185.161.6',
+            auth: 'keyMain',
+            authFile: '.ftppass',
+            port: 30000,
+            remotePath: '/var/zpanel/hostdata/zadmin/public_html/apflora_ch/php'
+        }));
+});
+
+gulp.task('sftp_', function () {
+    return gulp.src('index.html')
+        .pipe(sftp({
+            host: '146.185.161.6',
+            auth: 'keyMain',
+            authFile: '.ftppass',
+            port: 30000,
+            remotePath: '/var/zpanel/hostdata/zadmin/public_html/apflora_ch/'
+        }));
 });
