15197d983075509d7fda4e5b73be6266fa1db1e5
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-07-14 23:08:30 +0200

    jquery.filedownload aus apflora.js ausgelagert

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('dev_build_style', function() {
 });
 
 gulp.task('prod_build_src', function() {
-    return gulp.src(['src/jquery-ui.js', 'src/jquery.ui.touch-punch.js', 'src/jquery.cookie.js', 'src/jquery.hotkeys.js', 'src/hammer.js', 'src/jquery.hammer.js', 'src/markerclusterer.js', 'src/markerwithlabel.js', 'src/ruler.js', 'src/jsuri.js', 'src/jquery.qtip.js', 'src/proj4.js', 'src/apflora.js', 'src/jquery.file.download.js', 'src/underscore.js'])
+    return gulp.src(['src/jquery-ui.js', 'src/jquery.ui.touch-punch.js', 'src/jquery.cookie.js', 'src/jquery.hotkeys.js', 'src/hammer.js', 'src/jquery.hammer.js', 'src/markerclusterer.js', 'src/markerwithlabel.js', 'src/ruler.js', 'src/jsuri.js', 'src/jquery.qtip.js', 'src/proj4.js', 'src/jquery.fileDownload.js', 'src/apflora.js', 'src/jquery.file.download.js', 'src/underscore.js'])
         .pipe(concat('main.js'))
         .pipe(uglify())
         .pipe(gulp.dest('src'))
@@ -62,7 +62,7 @@ gulp.task('prod_build_src', function() {
 });
 
 gulp.task('dev_build_src', function() {
-    return gulp.src(['src/jquery-ui.js', 'src/jquery.ui.touch-punch.js', 'src/jquery.cookie.js', 'src/jquery.hotkeys.js', 'src/hammer.js', 'src/jquery.hammer.js', 'src/markerclusterer.js', 'src/markerwithlabel.js', 'src/ruler.js', 'src/jsuri.js', 'src/jquery.qtip.js', 'src/proj4.js', 'src/apflora.js', 'src/jquery.file.download.js', 'src/underscore.js'])
+    return gulp.src(['src/jquery-ui.js', 'src/jquery.ui.touch-punch.js', 'src/jquery.cookie.js', 'src/jquery.hotkeys.js', 'src/hammer.js', 'src/jquery.hammer.js', 'src/markerclusterer.js', 'src/markerwithlabel.js', 'src/ruler.js', 'src/jsuri.js', 'src/jquery.qtip.js', 'src/proj4.js', 'src/jquery.fileDownload.js', 'src/apflora.js', 'src/jquery.file.download.js', 'src/underscore.js'])
         .pipe(concat('main.js'))
         .pipe(gulp.dest('src'))
         .pipe(gulp.dest('../programme/xampp/htdocs/apflora/src'))
