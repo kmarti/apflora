fdda2e74c3e67ff3f3f4079365d06bdf125a0002
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-10-29 16:56:39 +0100

    apInsert hat zwei Verbindungen

--- queries/apInsert.js
+++ queries/apInsert.js
@@ -10,9 +10,15 @@ var mysql      = require('mysql'),
         user: config.db.userName,
         password: config.db.passWord,
         database: 'alexande_apflora'
+    }),
+    connection2 = mysql.createConnection({
+        host: 'localhost',
+        user: config.db.userName,
+        password: config.db.passWord,
+        database: 'alexande_beob'
     });
 
-var returnFunction = function (request, reply) {
+var returnFunction = function (request, callback) {
     var apId = decodeURIComponent(request.params.apId), // ApArtId
         user = decodeURIComponent(request.params.user), // der Benutzername
         date = new Date().toISOString();                // wann gespeichert wird
@@ -20,22 +26,24 @@ var returnFunction = function (request, reply) {
     async.parallel({
         insertIntoTblAktionsplan: function (callback) {
             connection.query(
-                'INSERT INTO tblAktionsplan (ApArtId, MutWann, MutWer) VALUES (' + apId + ', "' + date + '", "' + user + '")',
+                'INSERT INTO alexande_apflora.tblAktionsplan (ApArtId, MutWann, MutWer) VALUES (' + apId + ', "' + date + '", "' + user + '")',
                 function (err, data) {
+                    console.log('apInsert, insertIntoTblAktionsplan: data after insert = ', data);
                     callback(err, null);
                 }
             );
         },
         getArtwert: function (callback) {
-            connection.query(
+            connection2.query(
                 'SELECT Artwert FROM alexande_beob.ArtenDb_Arteigenschaften WHERE TaxonomieId=' + apId,
                 function (err, data) {
+                    console.log('apInsert, getArtwert: data after insert = ', data);
                     // keine Fehler melden, wenn bloss der Artwert nicht geholt wurde
                     if (data && data[0]) {
                         var artwert = data[0];
-                        callback(null, artwert);
+                        callback(err, artwert);
                     } else {
-                        callback(null, null);
+                        callback(err, null);
                     }
                 }
             );
@@ -44,8 +52,9 @@ var returnFunction = function (request, reply) {
         var artwert = results.getArtwert || null;
         if (artwert) {
             connection.query(
-                'UPDATE tblAktionsplan SET ApArtwert="' + artwert + '" WHERE ApArtId = ' + apId,
+                'UPDATE alexande_apflora.tblAktionsplan SET ApArtwert="' + artwert + '" WHERE ApArtId = ' + apId,
                 function (err, data) {
+                    console.log('apInsert, update Aktionsplan with artwert: data after update = ', data);
                     // nichts tun
                 }
             );
