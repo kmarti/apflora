18094393b029588450e5fab7cb5e1128565b0ab9
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-11-06 23:06:27 +0100

    modularisiert

--- src/apflora.js
+++ src/apflora.js
@@ -2106,54 +2106,9 @@ window.apf.olmap.initiiereLayertree = function (active_kategorie) {
     require('./modules/olmap/initiiereLayertree')(active_kategorie);
 };
 
-// Formulare als dialog öffnen
-// braucht die id des Formulars
-// und die id des Datensatzes, der anzuzeigen ist
+// wird in index.html verwendet
 window.apf.öffneFormularAlsPopup = function (formularname, id) {
-    var $formularname = $('#' + formularname),
-        title;
-
-    // id setzen
-    localStorage[formularname + '_id'] = id;
-
-    // titel bestimmen
-    switch (formularname) {
-    case 'pop':
-        title = 'Population';
-        // formular initiieren, ohne anzuzeigen
-        require('./modules/initiierePop')(null, id, true);
-        break;
-    case 'tpop':
-        title = 'Teilpopulation';
-        // formular initiieren, ohne anzuzeigen
-        require('./modules/initiiereTpop')(null, null, id, true);
-        break;
-    case 'beob':
-        title = 'Beobachtung';
-        // formular initiieren, ohne anzuzeigen
-        require('./modules/initiiereBeob')('beob_nicht_beurteilt', id, 'nicht_beurteilt', true);
-        break;
-    default:
-        title = '';
-    }
-
-    // dialog öffnen
-    $formularname.dialog({
-        close: function () {
-            $formularname.dialog("destroy");
-        },
-          //height: 600,
-        width: 600,
-        maxHeight: $('#menu').height(),
-        resizable: true,
-        position: {
-            my: 'left top',
-            at: 'left top',
-            of: $('#menu')
-        },
-        title: title
-    });
-    $formularname.dialog("open");
+    require('./modules/oeffneFormularAlsPopup')(formularname, id);
 };
 
 window.apf.olmap.detailplanStyle = function (feature, resolution) {
---
+++ src/modules/oeffneFormularAlsPopup.js
@@ -0,0 +1,57 @@
+// Formulare als dialog öffnen
+// braucht die id des Formulars
+// und die id des Datensatzes, der anzuzeigen ist
+
+/*jslint node: true, browser: true, nomen: true, todo: true */
+'use strict';
+
+var $ = require('jquery');
+
+var returnFunction = function (formularname, id) {
+    var $formularname = $('#' + formularname),
+        title;
+
+    // id setzen
+    localStorage[formularname + '_id'] = id;
+
+    // titel bestimmen
+    switch (formularname) {
+    case 'pop':
+        title = 'Population';
+        // formular initiieren, ohne anzuzeigen
+        require('./initiierePop')(null, id, true);
+        break;
+    case 'tpop':
+        title = 'Teilpopulation';
+        // formular initiieren, ohne anzuzeigen
+        require('./initiiereTpop')(null, null, id, true);
+        break;
+    case 'beob':
+        title = 'Beobachtung';
+        // formular initiieren, ohne anzuzeigen
+        require('./initiiereBeob')('beob_nicht_beurteilt', id, 'nicht_beurteilt', true);
+        break;
+    default:
+        title = '';
+    }
+
+    // dialog öffnen
+    $formularname.dialog({
+        close: function () {
+            $formularname.dialog("destroy");
+        },
+          //height: 600,
+        width: 600,
+        maxHeight: $('#menu').height(),
+        resizable: true,
+        position: {
+            my: 'left top',
+            at: 'left top',
+            of: $('#menu')
+        },
+        title: title
+    });
+    $formularname.dialog("open");
+};
+
+module.exports = returnFunction;
