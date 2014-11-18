b1e5246a72893eb62396e4ed4e1f82fe8cf50a7b
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-11-18 11:59:55 +0100

    modularisiert

---
+++ src/modules/jstree/loescheJber.js
@@ -0,0 +1,55 @@
+/*jslint node: true, browser: true, nomen: true, todo: true */
+'use strict';
+
+var $ = require('jquery'),
+    erstelleIdAusDomAttributId = require('../erstelleIdAusDomAttributId'),
+    melde                      = require('../melde'),
+    frageObUndeleteDatensatz   = require('../frageObUndeleteDatensatz');
+
+module.exports = function (aktiverNode, parentNode) {
+    var bezeichnung;
+
+    // nur aktualisieren, wenn Schreibrechte bestehen
+    if (!window.apf.pruefeSchreibvoraussetzungen()) {
+        return;
+    }
+    // selektieren, falls direkt mit der rechten Maustaste gewählt wurde
+    $.jstree._reference(aktiverNode).deselect_all();
+    // alle tieferen Knoten öffnen um zu zeigen, was mit gelöscht wird
+    $.jstree._reference(aktiverNode).open_all(aktiverNode);
+    $.jstree._reference(aktiverNode).deselect_all();
+    $.jstree._reference(aktiverNode).select_node(aktiverNode);
+    bezeichnung = $.jstree._reference(aktiverNode).get_text(aktiverNode);
+    $("#loeschen_dialog_mitteilung").html("Der AP-Bericht '" + bezeichnung + "' wird gelöscht.");
+    $("#loeschen_dialog").dialog({
+        resizable: false,
+        height:    'auto',
+        width:     400,
+        modal:     true,
+        buttons: {
+            "ja, löschen!": function () {
+                $(this).dialog("close");
+                // Variable zum rückgängig machen erstellen
+                window.apf.deleted     = window.apf.jber;
+                window.apf.deleted.typ = "jber";
+                $.ajax({
+                    type: 'delete',
+                    url: 'api/v1/apflora/tabelle=tblJBer/tabelleIdFeld=JBerId/tabelleId=' + erstelleIdAusDomAttributId($(aktiverNode).attr("id"))
+                }).done(function () {
+                    delete localStorage.jberId;
+                    delete window.apf.jber;
+                    $.jstree._reference(aktiverNode).delete_node(aktiverNode);
+                    // Parent Node-Beschriftung: Anzahl anpassen
+                    window.apf.beschrifteOrdnerJber(parentNode);
+                    // Hinweis zum rückgängig machen anzeigen
+                    frageObUndeleteDatensatz("Der AP-Bericht '" + bezeichnung + "' wurde gelöscht.");
+                }).fail(function () {
+                    melde("Fehler: Der AP-Bericht wurde nicht gelöscht");
+                });
+            },
+            "abbrechen": function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+};
--- src/modules/jstree/treeKontextmenu.js
+++ src/modules/jstree/treeKontextmenu.js
@@ -19,7 +19,8 @@ var _                                         = require('underscore'),
     loescheZielber                            = require('./loescheZielber'),
     insertNeuesErfkrit                        = require('./insertNeuesErfkrit'),
     loescheErfkrit                            = require('./loescheErfkrit'),
-    insertNeuenApber                          = require('./insertNeuenApber'),
+    insertNeuenJber                           = require('./insertNeuenJber'),
+    loescheJber                               = require('./loescheJber'),
     fuegeAusgeschnittenePopEin                = require('./fuegeAusgeschnittenePopEin'),
     zeigePopsAufOlmap                         = require('./zeigePopsAufOlmap'),
     zeigePopsAufGmap                          = require('./zeigePopsAufGmap');
@@ -207,7 +208,7 @@ var returnFunction = function (node) {
                 "label": "neuer AP-Bericht",
                 "icon":  "style/images/neu.png",
                 "action": function () {
-                    insertNeuenApber(aktiverNode, parentNode, $(aktiverNode).attr("id"));
+                    insertNeuenJber(aktiverNode, parentNode, $(aktiverNode).attr("id"));
                 }
             }
         };
@@ -217,57 +218,15 @@ var returnFunction = function (node) {
                 "label": "neuer AP-Bericht",
                 "icon": "style/images/neu.png",
                 "action": function () {
-                    insertNeuenApber(aktiverNode, parentNode, $(parentNode).attr("id"));
+                    insertNeuenJber(aktiverNode, parentNode, $(parentNode).attr("id"));
                 }
             },
             "loeschen": {
-                "label": "löschen",
+                "label":            "löschen",
                 "separator_before": true,
-                "icon": "style/images/loeschen.png",
+                "icon":             "style/images/loeschen.png",
                 "action": function () {
-                    // nur aktualisieren, wenn Schreibrechte bestehen
-                    if (!window.apf.pruefeSchreibvoraussetzungen()) {
-                        return;
-                    }
-                    // selektieren, falls direkt mit der rechten Maustaste gewählt wurde
-                    $.jstree._reference(aktiverNode).deselect_all();
-                    // alle tieferen Knoten öffnen um zu zeigen, was mit gelöscht wird
-                    $.jstree._reference(aktiverNode).open_all(aktiverNode);
-                    $.jstree._reference(aktiverNode).deselect_all();
-                    $.jstree._reference(aktiverNode).select_node(aktiverNode);
-                    var bezeichnung = $.jstree._reference(aktiverNode).get_text(aktiverNode);
-                    $("#loeschen_dialog_mitteilung").html("Der AP-Bericht '" + bezeichnung + "' wird gelöscht.");
-                    $("#loeschen_dialog").dialog({
-                        resizable: false,
-                        height:    'auto',
-                        width:     400,
-                        modal:     true,
-                        buttons: {
-                            "ja, löschen!": function () {
-                                $(this).dialog("close");
-                                // Variable zum rückgängig machen erstellen
-                                window.apf.deleted    = window.apf.jber;
-                                window.apf.deleted.typ = "jber";
-                                $.ajax({
-                                    type: 'delete',
-                                    url: 'api/v1/apflora/tabelle=tblJBer/tabelleIdFeld=JBerId/tabelleId=' + erstelleIdAusDomAttributId($(aktiverNode).attr("id"))
-                                }).done(function () {
-                                    delete localStorage.jberId;
-                                    delete window.apf.jber;
-                                    $.jstree._reference(aktiverNode).delete_node(aktiverNode);
-                                    // Parent Node-Beschriftung: Anzahl anpassen
-                                    window.apf.beschrifteOrdnerJber(parentNode);
-                                    // Hinweis zum rückgängig machen anzeigen
-                                    frageObUndeleteDatensatz("Der AP-Bericht '" + bezeichnung + "' wurde gelöscht.");
-                                }).fail(function () {
-                                    melde("Fehler: Der AP-Bericht wurde nicht gelöscht");
-                                });
-                            },
-                            "abbrechen": function () {
-                                $(this).dialog("close");
-                            }
-                        }
-                    });
+                    loescheJber(aktiverNode, parentNode);
                 }
             }
         };
