42962c1e6f64ce7d8c2476cc42a667a50e613fb6
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-11-18 10:53:00 +0100

    modularisiert

--- src/modules/jstree/treeKontextmenu.js
+++ src/modules/jstree/treeKontextmenu.js
@@ -14,6 +14,7 @@ var _                                         = require('underscore'),
     zeigePop                                  = require('../olmap/zeigePop'),
     insertNeuePop                             = require('./insertNeuePop'),
     insertNeuesApziel                         = require('./insertNeuesApziel'),
+    loescheApziel                             = require('./loescheApziel'),
     fuegeAusgeschnittenePopEin                = require('./fuegeAusgeschnittenePopEin'),
     zeigePopsAufOlmap                         = require('./zeigePopsAufOlmap'),
     zeigePopsAufGmap                          = require('./zeigePopsAufGmap');
@@ -124,60 +125,11 @@ var returnFunction = function (node) {
                 }
             },
             "loeschen": {
-                "label": "löschen",
+                "label":            "löschen",
                 "separator_before": true,
-                "icon": "style/images/loeschen.png",
+                "icon":             "style/images/loeschen.png",
                 "action": function () {
-                    // nur aktualisieren, wenn Schreibrechte bestehen
-                    if (!window.apf.pruefeSchreibvoraussetzungen()) {
-                        return;
-                    }
-                    // selektieren, falls direkt mit der rechten Maustaste gewählt wurde
-                    $.jstree._reference(aktiverNode).deselect_all();
-                    // alle tieferen Knoten öffnen um zu zeigen, was mit gelöscht wird
-                    $.jstree._reference(aktiverNode).open_all(aktiverNode);
-                    $.jstree._reference(aktiverNode).deselect_all();
-                    $.jstree._reference(aktiverNode).select_node(aktiverNode);
-                    var bezeichnung = $.jstree._reference(aktiverNode).get_text(aktiverNode);
-                    $("#loeschen_dialog_mitteilung").html("Das AP-Ziel '" + bezeichnung + "' wird gelöscht.");
-                    $("#loeschen_dialog").dialog({
-                        resizable: false,
-                        height:'auto',
-                        width: 400,
-                        modal: true,
-                        buttons: {
-                            "ja, löschen!": function () {
-                                $(this).dialog("close");
-                                // Variable zum rückgängig machen erstellen
-                                window.apf.deleted = window.apf.apziel;
-                                window.apf.deleted.typ = "apziel";
-                                var deleteApziel = $.ajax({
-                                    type: 'delete',
-                                    url: 'api/v1/apflora/tabelle=tblZiel/tabelleIdFeld=ZielId/tabelleId=' + erstelleIdAusDomAttributId($(aktiverNode).attr("id"))
-                                });
-                                deleteApziel.done(function () {
-                                    delete localStorage.apzielId;
-                                    delete window.apf.apziel;
-                                    $.jstree._reference(aktiverNode).delete_node(aktiverNode);
-                                    // grandparent Node-Beschriftung: Anzahl anpassen
-                                    grandparentNode = $.jstree._reference(parentNode)._get_parent(parentNode);
-                                    window.apf.beschrifteOrdnerApziel(grandparentNode);
-                                    // parent Node-Beschriftung: Anzahl anpassen
-                                    if ($.jstree._reference(parentNode).get_text(parentNode) !== "neue AP-Ziele") {
-                                        window.apf.beschrifteOrdnerApzieljahr(parentNode);
-                                    }
-                                    // Hinweis zum rückgängig machen anzeigen
-                                    frageObUndeleteDatensatz("Das AP-Ziel '" + bezeichnung + "' wurde gelöscht.");
-                                });
-                                deleteApziel.fail(function () {
-                                    melde("Fehler: Das AP-Ziel wurde nicht gelöscht");
-                                });
-                            },
-                            "abbrechen": function () {
-                                $(this).dialog("close");
-                            }
-                        }
-                    });
+                    loescheApziel(aktiverNode, parentNode);
                 }
             }
         };
