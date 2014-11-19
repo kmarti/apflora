b1243e62bc9b491d9f6daae98b71a938091cc347
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-11-19 21:13:05 +0100

    modularisiert

--- src/modules/jstree/treeKontextmenu.js
+++ src/modules/jstree/treeKontextmenu.js
@@ -37,6 +37,7 @@ var _                                         = require('underscore'),
     kopiereFreiwkontr                         = require('./kopiereFreiwkontr'),
     insertNeueMassnahme                       = require('./insertNeueMassnahme'),
     insertKopierteMassnahme                   = require('./insertKopierteMassnahme'),
+    loescheTpopmassn                          = require('./loescheTpopmassn'),
     insertNeuesApziel                         = require('./insertNeuesApziel'),
     loescheApziel                             = require('./loescheApziel'),
     insertNeuenZielber                        = require('./insertNeuenZielber'),
@@ -886,43 +887,7 @@ module.exports = function (node) {
                 "separator_before": true,
                 "icon":             "style/images/loeschen.png",
                 "action": function () {
-                    // nur aktualisieren, wenn Schreibrechte bestehen
-                    if (!window.apf.pruefeSchreibvoraussetzungen()) {
-                        return;
-                    }
-                    var bezeichnung = $.jstree._reference(aktiverNode).get_text(aktiverNode);
-                    $("#loeschen_dialog_mitteilung").html("Die Massnahme '" + bezeichnung + "' wird gelöscht.");
-                    $("#loeschen_dialog").dialog({
-                        resizable: false,
-                        height:    'auto',
-                        width:     400,
-                        modal:     true,
-                        buttons: {
-                            "ja, löschen!": function () {
-                                $(this).dialog("close");
-                                // Variable zum rückgängig machen erstellen
-                                window.apf.deleted     = window.apf.tpopmassn;
-                                window.apf.deleted.typ = "tpopmassn";
-                                $.ajax({
-                                    type: 'delete',
-                                    url: 'api/v1/apflora/tabelle=tblTeilPopMassnahme/tabelleIdFeld=TPopMassnId/tabelleId=' + erstelleIdAusDomAttributId($(aktiverNode).attr("id"))
-                                }).done(function () {
-                                    delete localStorage.tpopmassnId;
-                                    delete window.apf.tpopmassn;
-                                    $.jstree._reference(aktiverNode).delete_node(aktiverNode);
-                                    // Parent Node-Beschriftung: Anzahl anpassen
-                                    window.apf.beschrifteOrdnerTpopmassn(parentNode);
-                                    // Hinweis zum rückgängig machen anzeigen
-                                    frageObUndeleteDatensatz("Die Massnahme '" + bezeichnung + "' wurde gelöscht.");
-                                }).fail(function () {
-                                    melde("Fehler: Die Massnahme wurde nicht gelöscht");
-                                });
-                            },
-                            "abbrechen": function () {
-                                $(this).dialog("close");
-                            }
-                        }
-                    });
+                    loescheTpopmassn(aktiverNode, parentNode);
                 }
             }
         };
