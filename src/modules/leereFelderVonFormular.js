af36c0b18928f5f513533fd55f007de58e2e8a29
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2014-12-10 21:01:14 +0100

    leere Datumsfelder zeigten Datum des letzten offenen Datensatzes an. Korrigiert

--- src/modules/leereFelderVonFormular.js
+++ src/modules/leereFelderVonFormular.js
@@ -7,12 +7,15 @@ var $ = require('jquery');
 
 module.exports = function (formular) {
     $('#' + formular).find('input[type="text"]').each(function () {
-        $(this).val("");
+        $(this).val('');
+    });
+    $('#' + formular).find('input[type="date"]').each(function () {
+        $(this).val('');
     });
     $('#' + formular).find('input[type="radio"]:checked').each(function () {
         $(this).prop('checked', false);
     });
     $('#' + formular).find('select').each(function () {
-        $(this).val("");
+        $(this).val('');
     });
 };
